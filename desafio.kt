b783f7fc65cc65a64dbbc43b0b695883437ce4d1
Rodrigo Pyrrho Nascimento <rpyrrhow@gmail.com>
2023-09-22 11:03:02 -0300

    Desafio resolvido

--- desafio.kt
+++ desafio.kt
@@ -2,7 +2,7 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario (val nome: String, val id: Int) 
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
@@ -11,11 +11,27 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("Algusto", 1)
+    val usuario2 = Usuario("Bernado", 2)
+    val usuario3 = Usuario("Catarina", 3)
+    val usuario4 = Usuario("Diogo", 4)
+    val usuario5 = Usuario("Eliz", 5)
+    
+    val ce1 = ConteudoEducacional("conteudo educacional 1")
+    val ce2 = ConteudoEducacional("conteudo educacional 2")
+    
+    val ceList: List<ConteudoEducacional> = mutableListOf(ce1,ce2)
+    
+    val kotlin = Formacao("Kotlin",ceList)
+    
+    kotlin.matricular(usuario1)
+    kotlin.matricular(usuario2)
+    kotlin.matricular(usuario5)
+    
+    println(kotlin.inscritos);
 }
